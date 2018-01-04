430758beef3840f5078b5f180744c46669242d62
YanyangChen <184536632@qq.com>
2018-01-04 16:41:52 +0800

    testing with mock

---
+++ mokito-demo/src/test/java/com/cyy/mokito/mokitodemo/SomeBusinessMockTest.java
@@ -0,0 +1,30 @@
+package com.cyy.mokito.mokitodemo;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+
+public class SomeBusinessMockTest {
+
+	@Test
+	public void testFindTheGreatestFromAllData() {
+		DataService dataServiceMock = mock(DataService.class);
+		//create test data when retrieving function
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 24, 15, 3 });
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(24, result);
+	}
+
+	@Test
+	public void testFindTheGreatestFromAllData_ForOneValue() {
+		DataService dataServiceMock = mock(DataService.class);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 15 });
+		SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
+		int result = businessImpl.findTheGreatestFromAllData();
+		assertEquals(15, result);
+	}
+
+}
