312a791fc081d88bf9249b53be467711b8e9d492
YanyangChen <184536632@qq.com>
2018-01-04 15:58:51 +0800

    mokito unit test with stub

---
+++ mokito-demo/src/main/java/com/cyy/mokito/mokitodemo/DataService.java
@@ -0,0 +1,4 @@
+package com.cyy.mokito.mokitodemo;
+interface DataService{
+	int[] retrieveAllData();
+}
---
+++ mokito-demo/src/main/java/com/cyy/mokito/mokitodemo/MokitoDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cyy.mokito.mokitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoDemoApplication.class, args);
+	}
+}
